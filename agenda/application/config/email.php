fe5e10d5f7298f3cd4e932f829e307bdadbc4869
João Ladislau <126190669+Jladislauu@users.noreply.github.com>
2025-08-06 11:00:18 -0300

    configurações do email smtp feitas

--- agenda/application/config/email.php
+++ agenda/application/config/email.php
@@ -4,18 +4,18 @@
 // Example: $config['smtp_host'] = 'smtp.gmail.com';
 // @link https://codeigniter.com/user_guide/libraries/email.html
 
-$config['useragent'] = 'Easy!Appointments';
-$config['protocol'] = 'mail'; // or 'smtp'
+$config['useragent'] = 'RecepcionistAI';
+$config['protocol'] = 'smtp'; // or 'smtp'
 $config['mailtype'] = 'html'; // or 'text'
-// $config['smtp_debug'] = '0'; // or '1'
-// $config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
-// $config['smtp_host'] = '';
-// $config['smtp_user'] = '';
-// $config['smtp_pass'] = '';
-// $config['smtp_crypto'] = 'ssl'; // or 'tls'
-// $config['smtp_port'] = 25;
-// $config['from_name'] = '';
-// $config['from_address'] = '';
-// $config['reply_to'] = '';
+$config['smtp_debug'] = '0'; // or '1'
+$config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
+$config['smtp_host'] = 'smtp.gmail.com'; // Example: 'smtp.gmail.com'
+$config['smtp_user'] = 'recepcionistaistartup@gmail.com';
+$config['smtp_pass'] = 'mwacninmxmcmzdnv';
+$config['smtp_crypto'] = 'tls'; // or 'ssl'
+$config['smtp_port'] = 465;
+$config['from_name'] = 'RecepcionistAI';
+$config['from_address'] = 'recepcionistaistartup@gmail.com';
+$config['reply_to'] = 'recepcionistaistartup@gmail.com';
 $config['crlf'] = "\r\n";
 $config['newline'] = "\r\n";
